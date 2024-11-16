b8739552ffa5ea8d5454c45e4c9efd18f553e4aa
sanddot <sanginta@gmail.com>
2024-11-16 09:18:26 -0800

    Added forecast framework

--- src/Main.js
+++ src/Main.js
@@ -38,7 +38,69 @@ export default function Main() {
           <div className="celsius">ºC</div>
         </div>
       </div>
-      <div className="forecast">Forecast for 5 days</div>
+
+      <div className="forecast">
+        <div className="forecast-day">
+          <div className="day-of-week">Mon</div>
+          <div>
+            <img src={weatherData.imgUrl} className="day-emoji" />
+          </div>
+          <div className="temperature-wrapping">
+            <div className="day-high-temperature">
+              <strong>20º</strong>
+            </div>
+            <div class="day-low-temperature">5º</div>
+          </div>
+        </div>
+        <div className="forecast-day">
+          <div className="day-of-week">Tue</div>
+          <div>
+            <img src={weatherData.imgUrl} className="day-emoji" />
+          </div>
+          <div className="temperature-wrapping">
+            <div className="day-high-temperature">
+              <strong>21º</strong>
+            </div>
+            <div class="day-low-temperature">2º</div>
+          </div>
+        </div>
+        <div className="forecast-day">
+          <div className="day-of-week">Wed</div>
+          <div>
+            <img src={weatherData.imgUrl} className="day-emoji" />
+          </div>
+          <div className="temperature-wrapping">
+            <div className="day-high-temperature">
+              <strong>17º</strong>
+            </div>
+            <div class="day-low-temperature">0º</div>
+          </div>
+        </div>
+        <div className="forecast-day">
+          <div className="day-of-week">Thu</div>
+          <div>
+            <img src={weatherData.imgUrl} className="day-emoji" />
+          </div>
+          <div className="temperature-wrapping">
+            <div className="day-high-temperature">
+              <strong>22º</strong>
+            </div>
+            <div class="day-low-temperature">3º</div>
+          </div>
+        </div>
+        <div className="forecast-day">
+          <div className="day-of-week">Fri</div>
+          <div>
+            <img src={weatherData.imgUrl} className="day-emoji" />
+          </div>
+          <div className="temperature-wrapping">
+            <div className="day-high-temperature">
+              <strong>25º</strong>
+            </div>
+            <div class="day-low-temperature">7º</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
