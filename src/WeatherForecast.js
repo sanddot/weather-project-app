8d0383166e1e6da3a8d388236803853f8113cf45
sanddot <sanginta@gmail.com>
2024-11-27 18:31:11 -0800

    Added forecast day component and integrated

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,30 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
     console.log(response.data.coordinates);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = "t3f3af9b184481d0306edc82cbo6ff8c";
-  let longitude = props.coordinates.longitude;
-  let latitude = props.coordinates.latitude;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="forecast-day">
-        <div className="day-of-week">Mon</div>
-        <div className="forecast-icon">☀️</div>
-        <div className="temperature-wrapping">
-          <div className="day-high-temperature">
-            <strong>20º</strong>
-          </div>
-          <div className="day-low-temperature">5º</div>
-        </div>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <WeatherForecastDay data={forecast[0]} />
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "t3f3af9b184481d0306edc82cbo6ff8c";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function WeatherForecastDay(props) {
+  function dayHighTemperature() {
+    let highTemperature = Math.round(props.data.temperature.maximum);
+    return highTemperature;
+  }
+  function dayLowTemperature() {
+    let lowTemperature = Math.round(props.data.temperature.minimum);
+    return lowTemperature;
+  }
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+
+  return (
+    <div className="WeatherForecastDay">
+      <div className="forecast-day">
+        <div className="day-of-week">{day()}</div>
+        <div>
+          <img
+            className="forecast-icon"
+            src={props.data.condition.icon_url}
+            alt={props.data.condition.description}
+          />
+        </div>
+        <div className="temperature-wrapping">
+          <div className="day-high-temperature">
+            <strong>{dayHighTemperature()}º</strong>
+          </div>
+          <div className="day-low-temperature">{dayLowTemperature()}º</div>
+        </div>
+      </div>
+    </div>
+  );
+}
